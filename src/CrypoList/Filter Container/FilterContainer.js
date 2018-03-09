6166e10c86d3c23dfbed53b72cfcb3436b02c2bc
Nicholas Engleman <nicholas@englemaninvest.com>
2018-03-09 14:04:39 -0500

    add tweaks to button styles on hover

---
+++ src/CrypoList/Filter Container/FilterContainer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './FilterContainer.css';
+import './FilterItem/FilterItem';
+import FilterItem from "./FilterItem/FilterItem";
+
+class FilterContainer extends React.Component {
+	render() {
+		return (
+			<div className="filterContainer">
+				<div className="spaceMaker1"></div>
+				<div className="spaceMaker2"></div>
+				<FilterItem addFilter={this.props.addFilter} removeFilter={this.props.removeFilter} updateFilter={this.props.updateFilter} numberOfFilters={this.props.numberOfFilters} column="marketCap"/>
+				<FilterItem addFilter={this.props.addFilter} removeFilter={this.props.removeFilter} updateFilter={this.props.updateFilter} numberOfFilters={this.props.numberOfFilters} column="price"/>
+				<FilterItem addFilter={this.props.addFilter} removeFilter={this.props.removeFilter} updateFilter={this.props.updateFilter} numberOfFilters={this.props.numberOfFilters} column="Change1H"/>
+				<FilterItem addFilter={this.props.addFilter} removeFilter={this.props.removeFilter} updateFilter={this.props.updateFilter} numberOfFilters={this.props.numberOfFilters} column="Change24H"/>
+				<FilterItem addFilter={this.props.addFilter} removeFilter={this.props.removeFilter} updateFilter={this.props.updateFilter} numberOfFilters={this.props.numberOfFilters} column="Change7D"/>
+				<FilterItem addFilter={this.props.addFilter} removeFilter={this.props.removeFilter} updateFilter={this.props.updateFilter} numberOfFilters={this.props.numberOfFilters} column="Volume"/>
+			</div>
+		)
+	}
+}
+
+export default FilterContainer;
---
+++ src/CrypoList/Filter Container/FilterItem/FilterTypeButton/FilterTypeButton.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './FilterTypeButton.css';
+
+class FilterTypeButton extends React.Component {
+
+
+	buttonClass() {
+		if (this.props.selected) {
+			if (this.props.type === this.props.value) {
+				return 'typeButtons typeButtonSelected centerButton';
+			} else {
+				return 'hideButton';
+			}
+		} else {
+			if (this.props.type === this.props.value) {
+				return 'typeButtons typeButtonSelected';
+			} else {
+				return 'typeButtons';
+			}
+		}
+	}
+
+	render() {
+		return (
+			<button className={this.buttonClass()}
+					value={this.props.value}
+					onClick={this.props.clickEvent}>{this.props.value}
+					</button>
+		)
+	}
+}
+
+export default FilterTypeButton;
